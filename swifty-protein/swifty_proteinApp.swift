7d44cab0b7ec05cbbc20538b196e34f8ff13a94f
Lea Lescure <lea.lescure@hotmail.fr>
2023-09-26 18:26:02 +0200

    Initial Commit

---
+++ swifty-protein/swifty_proteinApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifty_proteinApp.swift
+//  swifty-protein
+//
+//  Created by Léa Lescure on 26/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct swifty_proteinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
