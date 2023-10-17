11ceddf7b2f63471bfcb37482a54fdc604319282
Lea Lescure <lea.lescure@hotmail.fr>
2023-10-17 17:40:14 +0200

    feat(save users): save users in userdefault

---
+++ swifty-protein/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  swifty-protein
+//
+//  Created by Léa Lescure on 17/10/2023.
+//
+
+import Foundation
+
+struct User: Codable {
+    let login: String
+    let password: String
+}
--- swifty-protein/Views/Login/LoginViewModel.swift
+++ swifty-protein/Views/Login/LoginViewModel.swift
@@ -17,7 +17,11 @@ final class LoginViewModel: ObservableObject {
     @Published var password: String = ""
     @Published var isTextFieldIsIncorrect: Bool = false
     @Published var shouldCheckTextField: Bool = false
-        
+    
+    private let userDefaults = UserDefaults.standard
+    private let userDefaultsKey: String = "users"
+    private var users: [User] = []
+    
     enum UserAuthenticationStatus {
         case loading
         case success
@@ -30,7 +34,14 @@ final class LoginViewModel: ObservableObject {
         case face
     }
     
-    init() { }
+    init() {
+        if let users = userDefaults.data(forKey: userDefaultsKey) {
+            if let decoded = try? JSONDecoder().decode([User].self, from: users) {
+                self.users = decoded
+                return
+            }
+        }
+    }
 }
 
 // MARK: - Biometry management
@@ -38,7 +49,7 @@ extension LoginViewModel {
     func checkBiometry() -> Bool {
         var error: NSError?
         let context = LAContext()
-
+        
         if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
             checkBiometryType(context)
             return true
@@ -63,7 +74,7 @@ extension LoginViewModel {
         let reason = "We need to unlock your data 😈"
         let context = LAContext()
         self.userAuthenticationStatus = .loading
-
+        
         context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, _ in
             Task { @MainActor in
                 if success {
@@ -79,13 +90,30 @@ extension LoginViewModel {
 }
 
 // MARK: - Textfield management
-
 extension LoginViewModel {
     func checkField() {
         if login.isEmpty || password.isEmpty && shouldCheckTextField {
             self.isTextFieldIsIncorrect = true
-            return 
+            return
         }
         self.isTextFieldIsIncorrect = false
+    }
+    
+    func authenticateManuallyUser(action: () -> Void) {
+        if users.contains { $0.login == self.login && $0.password == self.password } {
+            print("User already exists")
+            action()
+        } else {
+            users.append(User(login: login, password: password))
+            print("User created")
+            save()
+            action()
+        }
+    }
+    
+    func save() {
+        if let encoded = try? JSONEncoder().encode(users) {
+            userDefaults.set(encoded, forKey: userDefaultsKey)
+        }
     }
 }
